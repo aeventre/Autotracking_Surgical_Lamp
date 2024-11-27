e2defd65c3341572381d8d14b7148685f07574c8
Zackary Malmberg <zeke12268@gmail.com>
2024-11-26 22:43:53 -0500

    nothing changed

--- ros2_ws/src/hand_tracker/test/test_flake8.py
+++ ros2_ws/src/hand_tracker/test/test_flake8.py
@@ -22,4 +22,4 @@ def test_flake8():
     rc, errors = main_with_errors(argv=[])
     assert rc == 0, \
         'Found %d code style errors / warnings:\n' % len(errors) + \
-        '\n'.join(errors)
+        '\n'.join(errors) 
