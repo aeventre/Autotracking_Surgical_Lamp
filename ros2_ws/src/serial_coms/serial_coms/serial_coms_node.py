c464b2f670a62c7b2135ec736e79fa33c812bedd
Alec Ventresca <aeventre@buffalo.edu>
2025-02-25 22:41:38 -0500

    Implement serial communication in serial_coms_node and add subscription to joint_angles topic

--- ros2_ws/src/serial_coms/serial_coms/serial_coms_node.py
+++ ros2_ws/src/serial_coms/serial_coms/serial_coms_node.py
@@ -2,16 +2,28 @@ import rclpy
 from rclpy.node import Node
 from std_msgs import String
 
+import serial
+ser = serial.Serial('/dev/ttyUSB0')
 
-class Serial_Coms(Node):
+class SerialComs(Node):
 
     def __init__(self):
-        super.__init__('serial_coms')
-
+        super().__init__('serial_coms')
+        self.subscription = self.create_subscription(
+            String,
+            'joint_angles', # subscribes to joint angle topic
+            self.send_serial, # runs this function every time data is recieved
+            10)
+        self.subscription
+        
+    def send_serial(self,joint_angles):
+        ser.write(joint_angles)
+        
 
 def main(args=None):
+
     rclpy.init(args=args)
-    serial_coms = Serial_Coms()
+    serial_coms = SerialComs()
     rclpy.spin(serial_coms)
 
     serial_coms.destroy_node()
