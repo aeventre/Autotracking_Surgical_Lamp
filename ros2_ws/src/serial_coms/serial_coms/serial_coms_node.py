4088ed78cd6628c52e8c7b620d4bde9050769f3e
Alec Ventresca <aeventre@buffalo.edu>
2025-02-25 20:58:33 -0500

    add boilerplate code to serial_coms node

--- ros2_ws/src/serial_coms/serial_coms/serial_coms_node.py
+++ ros2_ws/src/serial_coms/serial_coms/serial_coms_node.py
@@ -7,3 +7,17 @@ class Serial_Coms(Node):
 
     def __init__(self):
         super.__init__('serial_coms')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    serial_coms = Serial_Coms()
+    rclpy.spin(serial_coms)
+
+    serial_coms.destroy_node()
+    rclpy.shutdown
+
+
+
+if __name__ == '__main__':
+    main()
