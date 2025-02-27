e8a5aff5ae13271872b5cd19b620912305af82a0
Alec Ventresca <aeventre@buffalo.edu>
2025-02-26 22:57:42 -0500

    Refactor 2D kinematics package: migrate to kinematics_2d and remove obsolete files

--- ros2_ws/src/kinematics_2d/setup.py
+++ ros2_ws/src/kinematics_2d/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            '2d_ik_node = 2d_kinematics.2d_ik_node:main',
+            'kinematics_2d = 2d_kinematics.ik_node_2d:main',
         ],
     },
 )
