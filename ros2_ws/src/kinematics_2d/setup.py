10d6eede0af78914b4bd65f22ab486dde2a04597
Alec Ventresca <aeventre@buffalo.edu>
2025-02-27 11:26:13 -0500

    Bump version to 0.0.1 and update package description; fix entry point name

--- ros2_ws/src/kinematics_2d/setup.py
+++ ros2_ws/src/kinematics_2d/setup.py
@@ -6,26 +6,23 @@ package_name = 'kinematics_2d'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='alec',
     maintainer_email='aeventre@buffalo.edu',
-    description='TODO: Package description',
+    description='ROS 2 kinematics package',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'kinematics_2d = 2d_kinematics.ik_node_2d:main',
+            'ik_node_2d = kinematics_2d.ik_node_2d:main',  # Fixed package name
         ],
     },
-        data_files=[
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-    ],
 )
