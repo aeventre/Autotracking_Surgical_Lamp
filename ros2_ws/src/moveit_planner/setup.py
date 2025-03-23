6001bb4d2842c22d34e951ecb259e869931b15ee
Alec Ventresca <aeventre@buffalo.edu>
2025-03-23 14:13:23 -0400

    Initial Structure for planner node

--- ros2_ws/src/moveit_planner/setup.py
+++ ros2_ws/src/moveit_planner/setup.py
@@ -1,25 +1,25 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'moveit_planner'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.0.1',
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/moveit_planner.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='alec',
-    maintainer_email='aeventre@buffalo.edu',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Motion planner node for the auto-tracking surgical lamp using MoveIt',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'moveit_planner_node = moveit_planner.moveit_planner_node:main',
         ],
     },
 )
