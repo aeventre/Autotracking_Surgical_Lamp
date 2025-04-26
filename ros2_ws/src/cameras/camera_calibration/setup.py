3af231c668f3f5e38c67653d35d5c9308258d1fa
Alec Ventresca <aeventre@buffalo.edu>
2025-04-25 20:31:21 -0400

    Implement camera calibration node and launch configuration; remove obsolete test files

--- ros2_ws/src/cameras/camera_calibration/setup.py
+++ ros2_ws/src/cameras/camera_calibration/setup.py
@@ -1,15 +1,18 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'camera_calibration'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'camera_calibrator_node = camera_calibration.camera_calibrator_node:main'
         ],
     },
 )
